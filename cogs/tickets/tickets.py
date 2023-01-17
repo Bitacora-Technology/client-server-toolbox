e080475ced2b3510ca532869e7a0bd6ed80e0b04
galind <guillem.yt1@gmail.com>
2023-01-17 04:52:32 +0100

    Added admin and guild only decorators to cog

--- cogs/tickets/tickets.py
+++ cogs/tickets/tickets.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
+from discord import app_commands
 import discord
 
 from bot import Bot
 
 
+@app_commands.guild_only()
+@app_commands.default_permissions(administrator=True)
 class Tickets(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
