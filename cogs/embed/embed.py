735e87a90ea7b8b2b99bf1a83176c695fb9c3a83
galind <guillem.yt1@gmail.com>
2023-01-18 14:45:55 +0100

    Added modal to send embed

--- cogs/embed/embed.py
+++ cogs/embed/embed.py
@@ -5,6 +5,33 @@ import discord
 from bot import Bot
 
 
+class SendEmbedModal(discord.ui.Modal):
+    def __init__(self, bot: Bot, channel: discord.TextChannel) -> None:
+        super().__init__(
+            title='Send Embed', timeout=None, custom_id='send-embed'
+        )
+        self.bot = bot
+        self.channel = channel
+
+    _title = discord.ui.TextInput(
+        label='Title', style=discord.TextStyle.short
+    )
+
+    description = discord.ui.TextInput(
+        label='Description', style=discord.TextStyle.long
+    )
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        embed = discord.Embed(
+            title=self._title.value, description=self.description.value,
+            color=self.bot.color
+        )
+        await self.channel.send(embed=embed)
+        await interaction.response.send_message(
+            f'Embed sent at <#{self.channel.id}>', ephemeral=True
+        )
+
+
 class Embed(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
@@ -14,3 +41,6 @@ class Embed(commands.Cog):
         self, interaction: discord.Interaction, channel: discord.TextChannel
     ) -> None:
         """Send an embedded message to the selected text channel"""
+        await interaction.response.send_modal(
+            SendEmbedModal(self.bot, channel)
+        )
