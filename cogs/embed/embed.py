0acdb5322c54b30f7810c416a06e716642534669
galind <guillem.yt1@gmail.com>
2023-01-18 14:37:39 +0100

    Added app command structure

--- cogs/embed/embed.py
+++ cogs/embed/embed.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import app_commands
 import discord
 
 from bot import Bot
@@ -7,3 +8,9 @@ from bot import Bot
 class Embed(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+
+    @app_commands.command(name='embed')
+    async def send_embed(
+        self, interaction: discord.Interaction, channel: discord.TextChannel
+    ) -> None:
+        """Send an embedded message to the selected text channel"""
